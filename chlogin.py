791a59b250c461d6a0f306face5659eb71a389ac
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-06 03:26:30 +0400

    chlogin.py

---
+++ chlogin.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#! -*- coding: utf-8 -*-
+
+print("Old login:")
+a = input()
+print("New login:")
+b = input()
+
+with open("~/Termux-OS/login.py", "r") as f:
+    raw = f.read().lower().replace(a, b)
+    file = open("~/Termux-OS/login.py", "w")
+    file.write(raw)
+    file.close()
+    f.close()
