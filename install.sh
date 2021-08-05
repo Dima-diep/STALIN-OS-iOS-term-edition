c7e1d7514dbca37d56d94b730d0d15890689f230
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-06 03:27:23 +0400

    install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+apk add python termux-api mpv htop-legacy neofetch -yq
+pip install termcolor
+termux-setup-storage
+echo "#!/bin/bash" > /bin/login
+echo "bash grub.sh && bash" >> /bin/login
+cd /bin
+touch grub.sh
+echo "#!/bin/bash" > grub.sh
+echo "python3 ~/Termux-OS/grub.py" >> grub.sh
+touch termuxos.sh
+echo "python3 ~/Termux-OS/chroot.py" > termuxos.sh
+chmod 777 termuxos.sh
+chmod 777 grub.sh
+cd -
+touch contacts.txt
+cd ~
+git clone https://github.com/Dima-diep/Music-Termux
+echo "GRUB has been installed. If you want run GRUB from loginned system, run 'bash grub.sh' command"
