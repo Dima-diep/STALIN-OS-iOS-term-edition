9c3ddf7c512c058b9e69298d4ed475f0e8319fba
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-08 00:52:45 +0400

    install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ echo "bash grub.sh && bash" >> /bin/login
 cd /bin
 touch grub.sh
 echo "#!/bin/bash" > grub.sh
-echo "python3 ~/Termux-OS/grub.py" >> grub.sh
+echo "python3 ~/Termux-OS-iOS/grub.py" >> grub.sh
 touch termuxos.sh
 echo "#!/bin/bash" > termuxos.sh
-echo "python3 ~/Termux-OS/chroot.py" >> termuxos.sh
+echo "python3 ~/Termux-OS-iOS/chroot.py" >> termuxos.sh
 chmod 777 termuxos.sh
 chmod 777 grub.sh
 cd -
