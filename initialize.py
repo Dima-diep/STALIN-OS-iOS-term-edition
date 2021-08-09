0f100b0b4b3d4b9bc3e422f297cf354592ea5164
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-09 15:30:31 +0400

    initialize.py

---
+++ initialize.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#! -*- coding: utf-8 -*-
+import os
+from termcolor import colored
+import time
+
+if os.file.exists("/home/Termux-OS/.recovery"):
+    print("What script do you want load to recovery?")
+    print("If your script isn't bash, then make bash script for running it!")
+    a = input()
+    os.system("mv " + a + " /home/Termux-OS/.recovery")
+    os.system("python3 ~/Termux-OS/chroot.py")
+else:
+    print(colored("WARNING! THE RECOVERY MODE ISN'T INITIALIZED", 'yellow', 'on_grey'))
+    time.sleep(2.5)
+    os.system("python3 ~/Termux-OS/chroot.py")
