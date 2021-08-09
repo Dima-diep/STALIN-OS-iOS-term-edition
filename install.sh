793ce36eaf768a8e2e2abc80227b617ed9c1ad0f
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-09 16:00:34 +0400

    install.sh

--- install.sh
+++ install.sh
@@ -4,18 +4,27 @@ pip install termcolor
 termux-setup-storage
 echo "#!/bin/bash" > /bin/login
 echo "bash grub.sh && bash" >> /bin/login
-cd /bin
-touch grub.sh
-echo "#!/bin/bash" > grub.sh
-echo "python3 ~/Termux-OS-iOS/grub.py" >> grub.sh
-touch termuxos.sh
-echo "#!/bin/bash" > termuxos.sh
-echo "python3 ~/Termux-OS-iOS/chroot.py" >> termuxos.sh
-chmod 777 termuxos.sh
-chmod 777 grub.sh
-cd -
-touch contacts.txt
 cd ~
+mkdir Termux-OS
+cd Termux-OS
+mkdir home
+cd ~
+echo "Move your files into new system..."
+mv * ~/Termux-OS/home
+cd ~/Termux-OS
+mkdir boot
+mkdir chroot
+mkdir .initialize
+mkdir temp
+cd chroot
+touch contacts.txt
+cd ../home/Termux-OS
+mv chroot.py ../../chroot
+mv grub.py ../../boot
+mv login.py ../../boot
+mv pass.py ../../boot
+mv initialize.sh ../../.initialize
+cd ../home
 git clone https://github.com/Dima-diep/Music-Termux
 chmod 777 ~/Music-Termux/*
-echo "Termux-OS has been installed.""
+echo "Termux-OS has been installed."
