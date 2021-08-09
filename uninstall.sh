8068c04346d349d9ee770512d0cd0a3d0aff16e6
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-09 16:09:00 +0400

    uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,8 +3,7 @@ echo "#!/bin/bash" > /bin/login
 echo "cat /etc/motd" >> /bin/login
 echo "bash" >> /bin/login
 cd ~/Termux-OS/home
-rm -rf ~/Termux-OS/home/Termux-OS-iOS
 rm -rf ~/Termux-OS/home/Music-Termux
 mv * ~/
 cd ~
-rm -rf Termux-OS
+rm -rf Termux-OS*
