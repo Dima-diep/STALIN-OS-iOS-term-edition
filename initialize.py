c08e416e01f6ad09db81e74b8447634e97dd5b87
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-10 23:43:42 +0400

    initialize.py

--- initialize.py
+++ initialize.py
@@ -4,13 +4,13 @@ import os
 from termcolor import colored
 import time
 
-if os.file.exists("/home/Termux-OS/.recovery"):
+if os.file.exists("/home/STALIN-OS/.recovery"):
     print("What script do you want load to recovery?")
     print("If your script isn't bash, then make bash script for running it!")
     a = input()
-    os.system("mv " + a + " /home/Termux-OS/.recovery")
-    os.system("python3 ~/Termux-OS/chroot.py")
+    os.system("mv " + a + " ~/STALIN-OS/.recovery")
+    os.system("python3 ~/STALIN-OS/chroot.py")
 else:
     print(colored("WARNING! THE RECOVERY MODE ISN'T INITIALIZED", 'yellow', 'on_grey'))
     time.sleep(2.5)
-    os.system("python3 ~/Termux-OS/chroot.py")
+    os.system("python3 ~/STALIN-OS/chroot.py")
