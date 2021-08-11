1b5d6921348162ac74abdd53c2ef73d30b3909f5
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-12 01:48:37 +0400

    pacman.py

---
+++ pacman.py
@@ -0,0 +1,337 @@
+#!/usr/bin/env python3
+#! -*- coding: utf-8 -*-
+import os
+import time
+from termcolor import colored
+
+os.system("clear")
+print(colored("Hello! This is STALIN OS term-edition Package Manager!", 'cyan', 'on_grey'))
+print(colored("version 1.0 by Dima-diep", 'cyan', 'on_grey'))
+print(colored("| === === === === |", 'grey', 'on_blue'))
+print(colored("| 1.Games         |", 'grey', 'on_blue'))
+print(colored("|-----------------|", 'grey', 'on_blue'))
+print(colored("| 2.Network Tools |", 'grey', 'on_blue'))
+print(colored("|-----------------|", 'grey', 'on_blue'))
+print(colored("| 3.Compilers     |", 'grey', 'on_blue'))
+print(colored("|-----------------|", 'grey', 'on_blue'))
+print(colored("| 4.text editors  |", 'grey', 'on_blue'))
+print(colored("|-----------------|", 'grey', 'on_blue'))
+print(colored("| 5.SMS-Bombers   |", 'grey', 'on_blue'))
+print(colored("|-----------------|", 'grey', 'on_blue'))
+print(colored("| 6.Other Tools   |", 'grey', 'on_blue'))
+print(colored("|-----------------|", 'grey', 'on_blue'))
+print(colored("| 7.exit          |", 'grey', 'on_blue'))
+print(colored("| === === === === |", 'grey', 'on_blue'))
+print(colored("Select your type:", 'cyan', 'on_grey'))
+a = int(input())
+
+if a == 1:
+    os.system("clear")
+    print(colored("| === === === === === === === === ===  |", 'grey', 'on_blue'))
+    print(colored("| 1.0verkill       | 24.nudoku         |", 'grey', 'on_blue'))
+    print(colored("|--------------------------------------|", 'grey', 'on_blue'))
+    print(colored("| 2.angband        | 25.open-adventure |", 'grey', 'on_blue'))
+    print(colored("|--------------------------------------|", 'grey', 'on_blue'))
+    print(colored("| 3.bastet         | 26.pacman         |", 'grey', 'on_blue'))
+    print(colored("|--------------------------------------|", 'grey', 'on_blue'))
+    print(colored("| 4.brogue         | 27.snake          |", 'grey', 'on_blue'))
+    print(colored("|--------------------------------------|", 'grey', 'on_blue'))
+    print(colored("| 5.cavez-of-phear | 28.solitaire      |", 'grey', 'on_blue'))
+    print(colored("|--------------------------------------|", 'grey', 'on_blue'))
+    print(colored("| 6.cboard         | 29.viletris       |", 'grey', 'on_blue'))
+    print(colored("|--------------------------------------|", 'grey', 'on_blue'))
+    print(colored("| 7.curse-of-war   |###################|", 'grey', 'on_blue'))
+    print(colored("|------------------|###################|", 'grey', 'on_blue'))
+    print(colored("| 8.dmagnetic      |###################|", 'grey', 'on_blue'))
+    print(colored("|------------------|###################|", 'grey', 'on_blue'))
+    print(colored("| 9.dopewars       |###################|", 'grey', 'on_blue'))
+    print(colored("|------------------|###################|", 'grey', 'on_blue'))
+    print(colored("| 10.frotz         |###################|", 'grey', 'on_blue'))
+    print(colored("|------------------|###################|", 'grey', 'on_blue'))
+    print(colored("| 11.glulxe        |###################|", 'grey', 'on_blue'))
+    print(colored("|------------------|###################|", 'grey', 'on_blue'))
+    print(colored("| 12.gnuchess      |###################|", 'grey', 'on_blue'))
+    print(colored("|------------------|###################|", 'grey', 'on_blue'))
+    print(colored("| 13.gnugo         |###################|", 'grey', 'on_blue'))
+    print(colored("|------------------|###################|", 'grey', 'on_blue'))
+    print(colored("| 14.gnushogi      |###################|", 'grey', 'on_blue'))
+    print(colored("|------------------|###################|", 'grey', 'on_blue'))
+    print(colored("| 15.gnuski        |###################|", 'grey', 'on_blue'))
+    print(colored("|------------------|###################|", 'grey', 'on_blue'))
+    print(colored("| 16.greed         |###################|", 'grey', 'on_blue'))
+    print(colored("|------------------|###################|", 'grey', 'on_blue'))
+    print(colored("| 17.lexter        |###################|", 'grey', 'on_blue'))
+    print(colored("|------------------|###################|", 'grey', 'on_blue'))
+    print(colored("| 18.moon-buggy    |###################|", 'grey', 'on_blue'))
+    print(colored("|------------------|###################|", 'grey', 'on_blue'))
+    print(colored("| 19.moria         |###################|", 'grey', 'on_blue'))
+    print(colored("|------------------|###################|", 'grey', 'on_blue'))
+    print(colored("| 20.myman         |###################|", 'grey', 'on_blue'))
+    print(colored("|------------------|###################|", 'grey', 'on_blue'))
+    print(colored("| 21.nethack       |###################|", 'grey', 'on_blue'))
+    print(colored("|------------------|###################|", 'grey', 'on_blue'))
+    print(colored("| 22.ninvaders     |###################|", 'grey', 'on_blue'))
+    print(colored("|------------------|###################|", 'grey', 'on_blue'))
+    print(colored("| 23.nsnake        |###################|", 'grey', 'on_blue'))
+    print(colored("| === === === === ===  === === === === |", 'grey', 'on_blue'))
+    print(colored("Select your game:", 'red', 'on_grey'))
+    b = int(input())
+
+    if b == 1:
+        os.system("apk add 0verkill && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 2:
+        os.system("apk add angband && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 3:
+        os.system("apk add bastet && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 4:
+        os.system("apk add brogue && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 5:
+        os.system("apk add cavez-of-phear && python3 ~/STALIN-OS/chroor/pacman.py")
+    elif b == 6:
+        os.system("apk add cboard && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 7:
+        os.system("apk add curse-of-war && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 8:
+        os.system("apk add dmagnetic && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 9:
+        os.system("apk add dopewars && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 10:
+        os.system("apk add frotz && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 11:
+        os.system("apk add glulxe && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 12:
+        os.system("apk add gnuchess && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 13:
+        os.system("apk add gnugo && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 14:
+        os.system("apk add gnushogi && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 15:
+        os.system("apk add gnuski && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 16:
+        os.system("apk add greed && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 17:
+        os.system("apk add lexter && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 18:
+        os.system("apk add moon-buggy && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 19:
+        os.system("apk add moria && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 20:
+        os.system("apk add myman && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 21:
+        os.system("apk add nethack && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 22:
+        os.system("apk add ninvaders && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 23:
+        os.system("apk add nsnake && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 24:
+        os.system("apk add nudoku && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 25:
+        os.system("apk add open-adventure && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 26:
+        os.system("apk add pacman4console && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 27:
+        os.system("apk add snake && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 28:
+        os.system("apk add solitaire && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif b == 29:
+        os.system("apk add viletris && python3 ~/STALIN-OS/chroot/pacman.py")
+
+elif a == 2:
+    os.system("clear")
+    print(colored("| === === === === === === === === === === |", 'grey', 'on_blue'))
+    print(colored("| 1.nmap+netcat                           |", 'grey', 'on_blue'))
+    print(colored("|-----------------------------------------|", 'grey', 'on_blue'))
+    print(colored("| 2.metasploit                            |", 'grey', 'on_blue'))
+    print(colored("|-----------------------------------------|", 'grey', 'on_blue'))
+    print(colored("| 3.apache                                |", 'grey', 'on_blue'))
+    print(colored("|-----------------------------------------|", 'grey', 'on_blue'))
+    print(colored("| 4.sqlite3 (for reading databases)       |", 'grey', 'on_blue'))
+    print(colored("|-----------------------------------------|", 'grey', 'on_blue'))
+    print(colored("| 5.postgresql (also need for metasploit) |", 'grey', 'on_blue'))
+    print(colored("| === === === === === === === === === === |", 'grey', 'on_blue'))
+    print(colored("Select your repo:", 'red', 'on_grey'))
+    c = int(input())
+
+    if c == 1:
+        os.system("apk add nmap nmap-ncat && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif c == 2:
+        os.system("cd ~/STALIN-OS/home && git clone https://github.com/Dima-diep/iOS-Termux && cd iOS-Termux/metasploit && chmod 777 * && sh metasploit.sh && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif c == 3:
+        os.system("cd ~/STALIN-OS/home && git clone https://github.com/Dima-diep/apache-iSH && cd apache-iSH && chmod 777 * && sh apache.sh && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif c == 4:
+        os.system("apk add sqlite && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif c == 5:
+        os.system("apk add postgresql && python3 ~/STALIN-OS/chroot/pacman.py")
+elif a == 3:
+    os.system("clear")
+    print(colored("|===  ===  === |", 'grey', 'on_blue'))
+    print(colored("| 1.ruby2      |", 'grey', 'on_blue'))
+    print(colored("|--------------|", 'grey', 'on_blue'))
+    print(colored("| 2.python 2.7 |", 'grey', 'on_blue'))
+    print(colored("|--------------|", 'grey', 'on_blue'))
+    print(colored("| 3.rust       |", 'grey', 'on_blue'))
+    print(colored("|--------------|", 'grey', 'on_blue'))
+    print(colored("| 4.swift      |", 'grey', 'on_blue'))
+    print(colored("|--------------|", 'grey', 'on_blue'))
+    print(colored("| 5.c/c++      |", 'grey', 'on_blue'))
+    print(colored("|--------------|", 'grey', 'on_blue'))
+    print(colored("| 6.golang     |", 'grey', 'on_blue'))
+    print(colored("|--------------|", 'grey', 'on_blue'))
+    print(colored("| 7.php        |", 'grey', 'on_blue'))
+    print(colored("|--------------|", 'grey', 'on_blue'))
+    print(colored("| 8.java       |", 'grey', 'on_blue'))
+    print(colored("|--------------|", 'grey', 'on_blue'))
+    print(colored("| 9.javascript |", 'grey', 'on_blue'))
+    print(colored("|===  ===  === |", 'grey', 'on_blue'))
+    print(colored("Select your compiler:", 'yellow', 'on_grey'))
+    d = int(input())
+
+    if d == 1:
+        os.system("cd ~ &&  wget -q https://hax4us.github.io/TermuxBlack/termuxblack.key -O termuxblack.key && apk-key add termuxblack.key && apk update && apk add ruby2 && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif d == 2:
+        os.system("apk add python2 && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif d == 3:
+        os.system("apk add rust && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif d == 4:
+        os.system("apk add swift && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif d == 5:
+        os.system("apk add libllvm llvm && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif d == 6:
+        os.system("apk add golang && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif d == 7:
+        os.system("apk add php php-apache && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif d == 8:
+        os.system("apk add openjdk-17 && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif d == 9:
+        os.system("apk add nodejs nodejs-lts && python3 ~/STALIN-OS/chroot/pacman.py")
+elif a == 4:
+    os.system("clear")
+    print(colored("| ===  === |", 'grey', 'on_blue'))
+    print(colored("| 1.nano   |", 'grey', 'on_blue'))
+    print(colored("|----------|", 'grey', 'on_blue'))
+    print(colored("| 2.vim    |", 'grey', 'on_blue'))
+    print(colored("|----------|", 'grey', 'on_blue'))
+    print(colored("| 3.neovim |", 'grey', 'on_blue'))
+    print(colored("|----------|", 'grey', 'on_blue'))
+    print(colored("| 4.joe    |", 'grey', 'on_blue'))
+    print(colored("|----------|", 'grey', 'on_blue'))
+    print(colored("| 5.emacs  |", 'grey', 'on_blue'))
+    print(colored("|----------|", 'grey', 'on_blue'))
+    print(colored("| 6.micro  |", 'grey', 'on_blue'))
+    print(colored("| ===  === |", 'grey', 'on_blue'))
+    print(colored("Select your editor:", 'green', 'on_grey'))
+    e = int(input())
+
+    if e == 1:
+        os.system("apk add nano && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif e == 2:
+        os.system("apk add vim && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif e == 3:
+        os.system("apk add neovim && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif e == 4:
+        os.system("apk add joe && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif e == 5:
+        os.system("apk add emacs && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif e == 6:
+        os.system("apk add micro && python3 ~/STALIN-OS/chroot/pacman.py")
+elif a == 5:
+    os.system("clear")
+    print(colored("|  === ===  ===  === === |", 'grey', 'on_blue'))
+    print(colored("| 1.T-Bomb               |", 'grey', 'on_blue'))
+    print(colored("|------------------------|", 'grey', 'on_blue'))
+    print(colored("| 2.SMSBomber300         |", 'grey', 'on_blue'))
+    print(colored("|------------------------|", 'grey', 'on_blue'))
+    print(colored("| 3.Egyptian-SMS-Spammer |", 'grey', 'on_blue'))
+    print(colored("|------------------------|", 'grey', 'on_blue'))
+    print(colored("| 4.spymer               |", 'grey', 'on_blue'))
+    print(colored("| === ===  ===  === ===  |", 'grey', 'on_blue'))
+    print(colored("Select your bomber:", 'red', 'on_blue'))
+    f = int(input())
+
+    if f == 1:
+        os.system("cd ~/STALIN-OS/home && git clone https://github.com/TheSpeedX/TBomb && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif f == 2:
+        os.system("cd ~/STALIN-OS/home && git clone https://github.com/Ivan-Hacker-700/SMS-Bomber-300-Free && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif f == 3:
+        os.system("cd ~/STALIN-OS/home && git clone https://github.com/KendoClaw1/Egyptian-SMS-Spammer && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif f == 4:
+        os.system("cd ~/STALIN-OS/home && git clone https://github.com/FSystem88/spymer && python3 ~/STALIN-OS/chroot/pacman.py")
+
+elif a == 6:
+    os.system("clear")
+    print(colored("| === === === === === === |", 'grey', 'on_blue'))
+    print(colored("| 1.Telegram Number Check |", 'grey', 'on_blue'))
+    print(colored("|-------------------------|", 'grey', 'on_blue'))
+    print(colored("| 2.system upgrade        |", 'grey', 'on_blue'))
+    print(colored("|-------------------------|", 'grey', 'on_blue'))
+    print(colored("| 3.Install your package  |", 'grey', 'on_blue'))
+    print(colored("|-------------------------|", 'grey', 'on_blue'))
+    print(colored("| 4.Clone your repository |", 'grey', 'on_blue'))
+    print(colored("|-------------------------|", 'grey', 'on_blue'))
+    print(colored("| 5.Create your package   |", 'grey', 'on_blue'))
+    print(colored("|-------------------------|", 'grey', 'on_blue'))
+    print(colored("| 6.Remove your packages  |", 'grey', 'on_blue'))
+    print(colored("| === === === === === === |", 'grey', 'on_blue'))
+    print(colored("Select your function:", 'green', 'on_grey'))
+    g = int(input())
+
+    if g == 1:
+        os.system("cd ~/STALIN-OS/home && git clone https://github.com/Dima-diep/Telegram-numcheck && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif g == 2:
+        os.system("apk update && apk upgrade && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif g == 3:
+        print("Write list:" )
+        h = input()
+        os.system("apk add " + h + " && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif g == 4:
+        print("1.Git by link")
+        print("2.Git by author/repo")
+        print("3.Write command for cloning")
+        i = input()
+
+        if i == 1:
+            print("Get your link:")
+            j = input()
+            os.system("cd ~/STALIN-OS/home && git clone " + j + " && python3 ~/STALIN-OS/chroot/pacman.py")
+        elif i == 2:
+            print("Get author/repo:")
+            k = input()
+            os.system("cd ~/STALIN-OS/home && git clone https://github.com/" + k + " && python3 ~/STALIN-OS/chroot/pacman.py")
+        elif i == 3:
+            print("Get your command:")
+            l = input()
+            os.system("cd ~ && " + l + " && python3 ~/STALIN-OS/chroot/pacman.py")
+    elif g == 5:
+        print("Directory with files for your package (full path):")
+        m = input()
+        os.system("cd " + m + " && cp ~/STALIN-OS/system/Manifest.json " + m)
+        print("Program filename:")
+        n = input()
+        os.system("touch " + n)
+        print("Run your favourite editor, then edit Manifest.json")
+        o = input()
+        os.system(o + " " + n + " && " + o + " Manifest.json && termux-create-package Manifest.json")
+        print("Install this package? y/n")
+        p = input()
+
+        if p == 'y':
+            os.system("dpkg -i *.deb && python3 ~/STALIN-OS/chroot/pacman.py")
+        elif p == 'n':
+            os.system("~/STALIN-OS/chroot/pacman.py")
+    elif g == 6:
+        print("Write list your packages:")
+        r = input()
+        os.system("apk remove " + r + " && apk autoremove && ~/STALIN-OS/chroot/pacman.py")
+elif a == 7:
+    os.system("clear")
+    print(colored("| ===  === === === |", 'grey', 'on_white'))
+    print(colored("| 1.Exit to chroot |", 'grey', 'on_white'))
+    print(colored("|------------------|", 'grey', 'on_white'))
+    print(colored("| 2.Exit to GRUB   |", 'grey', 'on_white'))
+    print(colored("| ===  === === === |", 'grey', 'on_white'))
+    q = int(input())
+
+    if q == 1:
+        os.system("python3 ~/STALIN-OS/chroot/chroot.py")
+    elif q == 2:
+        os.system("login")
